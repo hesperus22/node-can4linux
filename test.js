6de6e67dcbb4ccaf9cb1ac4fd96c7048c2c86ed2
Przemko Robakowski <przemko@przemko-robakowski.pl>
2016-05-06 20:17:46 +0200

    Test.js improvements

--- test.js
+++ test.js
@@ -1,12 +1,10 @@
 var Can = require('./can.js');
 
-var can = new Can({self:true});
+var can = new Can({device: process.argv[2], self:true});
 can.on('data', function(data){
     console.log(data);
 });
 can.on('error', function(err){
     console.log(err);
 });
-
-can.send({id: 16285706, ext:1});
 
